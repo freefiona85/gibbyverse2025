430e1876b64441c991e3ac6a9510cd93359e6529
Fiorina Liberta <freefiona85@gmail.com>
2025-06-30 17:49:06 +0700

    pushed the build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/3D-art-gallery',
+  base: '/',
   build: {
     rollupOptions: {
-      input: '/main.js',
+      input: {
+		  main: '/main.js',
+	  index: 'index.html',
+	  }
     },
   },
 });
